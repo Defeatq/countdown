07d325f8697631ac2cb19386c71284f594958e64
Artem <awrer1234eads@gmail.com>
2022-01-30 02:58:42 +0300

    Update

--- js/view.js
+++ js/view.js
@@ -1,11 +1,10 @@
+import { formatDuration } from 'date-fns';
+
 export const UI_ELEMENTS = {
 	COUNTDOWN_TIME: document.querySelector('.countdown__body'),
 	COUNTDOWN_DISPLAY: document.querySelector('.countdown__display'),
 };
 
-export function renderTime(time) {
-	UI_ELEMENTS.COUNTDOWN_DISPLAY.textContent = `
-  Years: ${time.years}\nMonths: ${time.months}
-  Days: ${time.days}\nHours: ${time.hours}
-  Minutes: ${time.minutes}\nSeconds: ${time.seconds}`;
+export function renderTime(timeUnits) {
+	UI_ELEMENTS.COUNTDOWN_DISPLAY.textContent = formatDuration(timeUnits);
 }
